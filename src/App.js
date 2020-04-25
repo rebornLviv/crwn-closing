0857e8c3eaf95d23ed794de7db2e525018489a63
Yura <bloadevil@gmail.com>
2020-04-25 07:32:06 +0300

    start auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import Authorization from './pages/authorization/authorization.component';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
       <Route exact  path='/' component={HomePage} />
       <Route   path='/shop' component={ShopPage} />
+      <Route   path='/signin' component={Authorization} />
       </Switch>
       {/* <HomePage /> */}
     </div>
---
+++ src/pages/authorization/authorization.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './authorization.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const Authorization =() =>(
+<div className='sign-in-and-sign-up' >
+    <SignIn />
+</div>
+
+)
+
+
+export default Authorization;
